1c8ff70cdf006713e4ffb21aee2a5c7808f3c83c
Tanmay04 <tanmaysafal.4@gmail.com>
2023-04-21 15:50:18 +0530

    Logging and exception

---
+++ src/logger.py
@@ -0,0 +1,21 @@
+import logging as lg
+import os
+from datetime import datetime
+
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
+
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+lg.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=lg.INFO
+
+)
+
+if __name__=="__main__":
+    lg.info("Logging has started")
+
